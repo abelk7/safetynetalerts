cece09c55851ecb8e3950b9dfcb47fddfcdf1fd6
Abel.K <kisebela.a@gmail.com>
2022-05-28 18:01:55 +0200

    Model en traduisant les données

---
+++ src/main/java/com/safetynet/safetynetalerts/model/FireStation.java
@@ -0,0 +1,24 @@
+package com.safetynet.safetynetalerts.model;
+
+public class FireStation {
+
+    private String address;
+    private Integer station;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getStation() {
+        return station;
+    }
+
+    public void setStation(Integer station) {
+        this.station = station;
+    }
+
+}
---
+++ src/main/java/com/safetynet/safetynetalerts/model/Person.java
@@ -0,0 +1,71 @@
+package com.safetynet.safetynetalerts.model;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String ville;
+    private String zip;
+    private String phone;
+    private String mail;
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String adress) {
+        this.address = adress;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+
+}
